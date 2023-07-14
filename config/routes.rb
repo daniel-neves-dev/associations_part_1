b76e493c41dc5ab5fe0cb022118c42dfb99a2c81
daniel-neves-dev <daniel.oliveira.au@hotmail.com>
2023-07-14 15:07:50 -0300

    feat: routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,5 @@
 Rails.application.routes.draw do
+  root 'pages#index'
   resources :books
   resources :authors
-  get 'pages/index'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
 end
