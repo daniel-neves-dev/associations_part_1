c38ae53e0956a9fe304cb484c463425fc0c617b3
daniel-neves-dev <daniel.oliveira.au@hotmail.com>
2023-07-14 15:02:33 -0300

    feat: author scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :authors
+  get 'pages/index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
