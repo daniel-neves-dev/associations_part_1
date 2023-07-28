cfa16698c713c1f04e2402bc1844da49d5681a26
daniel-neves-dev <daniel.oliveira.au@hotmail.com>
2023-07-28 13:05:01 -0300

    chore: image files

--- app/models/book.rb
+++ app/models/book.rb
@@ -6,7 +6,7 @@ class Book < ApplicationRecord
   has_one_attached :image
 
   def image_resize
-    return unless image.content_type.in?(%w[image/jpeg image/png])
+    return unless image.content_type.in?(%w[image/jpeg image/png image/psd image/bmp])
     image.variant(resize_to_limit:[300,300]).processed
   end
 end
