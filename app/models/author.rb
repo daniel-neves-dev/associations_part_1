abc5713b54c61da667b1d0fbeebe9aa9a9fa4ca4
daniel-neves-dev <daniel.oliveira.au@hotmail.com>
2023-08-08 17:32:09 -0300

    feat: identity

--- app/models/author.rb
+++ app/models/author.rb
@@ -8,6 +8,6 @@ class Author < ApplicationRecord
   def avatar_resize
     return unless avatar.content_type.in?(%w[image/jpeg image/png image/psd image/bmp])
 
-    avatar.variant(resize_to_limit: [100,100])
+    avatar.variant(resize_to_limit: [400,400])
   end
 end
